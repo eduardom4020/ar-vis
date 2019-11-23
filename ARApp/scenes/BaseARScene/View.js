951b19a2371f93f698d467a3ea4cf84af328f870
Eduardo Braga <eduardo.braga@delfosim.com>
2019-11-23 17:28:38 -0300

    feature: made base implementation of line chart

--- ARApp/scenes/BaseARScene/View.js
+++ ARApp/scenes/BaseARScene/View.js
@@ -8,15 +8,35 @@ import {
     ViroImage,
     ViroPolyline,
     ViroNode,
-    ViroSphere
+    ViroSphere,
+    ViroMaterials
 } from 'react-viro';
 
 import LogOBJ from '../../assets/log.obj';
-import Chart from '../../assets/chart-test.png';
-console.log(Chart)
+// import Chart from '../../assets/chart-test.png';
+// console.log(Chart)
 let timeout = null;
 
+ViroMaterials.createMaterials({
+    line: {
+        shininess: 1.0,
+        diffuseColor: '#429bf5'
+    //   lightingModel: "Lambert",
+    },
+    lineDot: {
+        shininess: 5.0,
+        diffuseColor: '#429bf5'
+    },
+    axis: {
+        diffuseColor: '#000000'
+    }
+});
+
 const SERIES = [ [0,0,0], [.25,.5,0], [.5,.15,0], [.75,.25,0], [1,1,0] ];
+const xAxisRange = [0, 1];
+const xAxisTicks = 5;
+const yAxisRange = [0, 1];
+const yAxisTicks = 5;
 
 const View = props => {
     const { sceneNavigator } = props;
@@ -53,7 +73,7 @@ const View = props => {
                             // position={[0,0,-2]} 
                             points={SERIES} 
                             thickness={0.005} 
-                            // materials='red' 
+                            materials='line' 
                         />
                         {
                             SERIES && SERIES.map(point => (
@@ -62,7 +82,68 @@ const View = props => {
                                     widthSegmentCount={20}
                                     radius={0.025}
                                     position={point}
+                                    materials='lineDot'
                                 />
+                            ))
+                        }
+                        {
+                            xAxisRange &&
+                                <ViroPolyline 
+                                    // position={[0,0,-2]} 
+                                    points={xAxisRange.map(xVal => [xVal, -.05, 0])} 
+                                    thickness={0.001} 
+                                    materials='axis' 
+                                />
+                        }
+                        {
+                            xAxisRange && new Array(xAxisTicks + 1).fill(Number((xAxisRange[1] - xAxisRange[0]) / xAxisTicks)).map((tick, index) => (
+                                <ViroNode
+                                    position={[tick * index, -.05, 0]}
+                                >
+                                    <ViroPolyline 
+                                        // position={[0,0,-2]} 
+                                        points={[[0, 0, 0], [0, .01, 0]]} 
+                                        thickness={0.002} 
+                                        materials='axis' 
+                                    />
+                                    <ViroText 
+                                        text={String(tick * index).slice(0, 4)}
+                                        scale={[.2, .2, .2]}
+                                        position={[0, -.1, 0]}
+                                        textAlign='center'
+                                        color='#000000'
+                                    />
+                                </ViroNode>
+                            ))
+                        }
+                        {
+                            yAxisRange &&
+                                <ViroPolyline 
+                                    // position={[0,0,-2]} 
+                                    points={yAxisRange.map(yVal => [-.05, yVal, 0])} 
+                                    thickness={0.001} 
+                                    materials='axis' 
+                                />
+                        }
+                        {
+                            yAxisRange && new Array(yAxisTicks + 1).fill(Number((yAxisRange[1] - yAxisRange[0]) / yAxisTicks)).map((tick, index) => (
+                                <ViroNode
+                                    position={[-.05, tick * index, 0]}
+                                >
+                                    <ViroPolyline 
+                                        // position={[0,0,-2]} 
+                                        points={[[0, 0, 0], [.01, 0, 0]]} 
+                                        thickness={0.002} 
+                                        materials='axis'  
+                                    />
+                                    <ViroText 
+                                        text={String(tick * index).slice(0, 4)}
+                                        scale={[.2, .2, .2]}
+                                        position={[-.12, -.08, 0]}
+                                        textAlign='right'
+                                        color='#000000'
+                                    />
+                                </ViroNode>
                             ))
                         }
                     </ViroNode>
