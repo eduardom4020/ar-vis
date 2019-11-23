05ca9aadcd1fcce5c605c6dfc35384a4414fae8e
Eduardo Braga <eduardo.braga@delfosim.com>
2019-11-23 10:42:33 -0300

    feature: restarted project development. After testing qr code scanners and third party data vis libs, like highcharts, made the choise to create own library using only Viro React

--- ARApp/App.js
+++ ARApp/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { ViroARSceneNavigator } from 'react-viro';
 import BaseARScene from './scenes/BaseARScene';
+import { RNCamera } from 'react-native-camera';
 
 const API_KEY = '3DB06101-0AB9-43F0-9F43-3F3CDC9F3BE8';
 
--- ARApp/scenes/BaseARScene/View.js
+++ ARApp/scenes/BaseARScene/View.js
@@ -4,14 +4,22 @@ import {
     ViroText,
     ViroConstants,
     Viro3DObject,
-    ViroAmbientLight
+    ViroAmbientLight,
+    ViroImage,
+    ViroPolyline,
+    ViroNode,
+    ViroSphere
 } from 'react-viro';
 
 import LogOBJ from '../../assets/log.obj';
-
+import Chart from '../../assets/chart-test.png';
+console.log(Chart)
 let timeout = null;
 
+const SERIES = [ [0,0,0], [.25,.5,0], [.5,.15,0], [.75,.25,0], [1,1,0] ];
+
 const View = props => {
+    const { sceneNavigator } = props;
     const [ text, setText ] = useState();
 
     const onTrackingUpdated = (state, reason) => {
@@ -21,21 +29,51 @@ const View = props => {
                 timeout = null;
             }
             
-            setText('Find Some QR Code');
+            setText('Chart Test!');
         } else if (state == ViroConstants.TRACKING_NONE && !timeout){
             timeout = setTimeout(() => setText(null), 300);
         }
     }
 
     return (
-        <ViroARScene onTrackingUpdated={onTrackingUpdated} >
+        <ViroARScene onTrackingUpdated={onTrackingUpdated} >           
             <ViroAmbientLight color="#FFFFFF" />
             <ViroText 
-                text={text || 'Tracking'}
+                text={text || 'Loading Scene'}
                 scale={[.5, .5, .5]} 
-                position={[0, .1, -0.6]} 
+                position={[0, .15, -0.6]} 
             />
-            <Viro3DObject 
+            {
+                text && 
+                    <ViroNode
+                        position={[-0.17, -.15, -0.6]}
+                        scale={[.25, .25, .25]}
+                    >
+                        <ViroPolyline 
+                            // position={[0,0,-2]} 
+                            points={SERIES} 
+                            thickness={0.005} 
+                            // materials='red' 
+                        />
+                        {
+                            SERIES && SERIES.map(point => (
+                                <ViroSphere
+                                    heightSegmentCount={20}
+                                    widthSegmentCount={20}
+                                    radius={0.025}
+                                    position={point}
+                                />
+                            ))
+                        }
+                    </ViroNode>
+            }
+            {/* <ViroImage
+                height={.5}
+                width={.5}
+                source={Chart}
+                position={[0.3, 0, -0.4]} 
+            /> */}
+            {/* <Viro3DObject 
                 source={LogOBJ}
                 highAccuracyEvents={true}
                 position={[-0.3, 0, -0.2]}
@@ -43,8 +81,8 @@ const View = props => {
                 rotation={[90, 0, 0]}
                 type='OBJ'
                 // transformBehaviors={['billboard']}
-                onClick={(position, source) => console.log('Bt click!')}
-            />
+                onClick={(position, source) => console.log('Took screenshot!') || sceneNavigator.takeScreenshot('test.jpg', true)}
+            /> */}
         </ViroARScene>
     );
 };
